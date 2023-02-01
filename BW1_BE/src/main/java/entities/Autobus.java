0e5990b4046cd8fcedaece87e937f7065a563103
Flavio Cirillo <113441260+FlavioCir@users.noreply.github.com>
2023-02-01 15:09:22 +0100

    Add files via upload

---
+++ BW1_BE/src/main/java/entities/Autobus.java
@@ -0,0 +1,37 @@
+package entities;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import entities.abstracts.Mezzo;
+import enums.TappeAutobus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "autobus")
+@DiscriminatorValue("Autobus")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Autobus extends Mezzo {
+
+	@Enumerated(EnumType.STRING)
+	private TappeAutobus partenza;
+	@Enumerated(EnumType.STRING)
+	private TappeAutobus capolinea;
+	
+	private String numero;
+	
+	public Autobus(int capienza, boolean inServizio, String numero, TappeAutobus partenza, TappeAutobus capolinea) {
+		super(capienza, inServizio);
+		this.numero = numero;
+		this.partenza = partenza;
+		this.capolinea = capolinea;
+	}
+	
+}
